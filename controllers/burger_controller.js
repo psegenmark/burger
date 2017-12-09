0be587de8941e76026551945303a79e5fbc4bc6b
psegenmark <psegenmark@gmail.com>
2017-12-09 13:59:47 -0800

    done

---
+++ controllers/burger_controller.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+
+var burger = require("../models/burger.js");
+
+app.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+

0f7008dbb49860b994cf17acbe2bce10adcc0d1f
psegenmark <psegenmark@gmail.com>
2017-12-09 15:12:57 -0800

    update

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,8 @@ var connection = mysql.createConnection({
   host: "us-cdbr-iron-east-05.cleardb.net",
   user: "b6632f3eab2253",
   password: "34e26a78",
-  database: "heroku_b4a67a1ba7cc760"
+  database: "heroku_b4a67a1ba7cc760",
+  port: 3306
 });
 
 connection.connect(function(err) {

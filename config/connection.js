54aeefaae78c590fd7224f266d3de530d6bb351c
psegenmark <psegenmark@gmail.com>
2017-12-09 16:57:01 -0800

    herokuu

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,12 @@
 var mysql = require("mysql");
 
-mysql://b9e28e3df8d24d:f004ae51@us-cdbr-iron-east-05.cleardb.net/heroku_83af1219e198553?reconnect=true
+
 var connection = mysql.createConnection({
+  driver: "mysql",
+  charset: "utf8",
+  collation: "utf8_unicode_ci",
+  prefix: "",
+  strict: false,
   host: "us-cdbr-iron-east-05.cleardb.net",
   user: "b9e28e3df8d24d",
   password: "f004ae51",

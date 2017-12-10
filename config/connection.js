9f422a9d116cf10513f34c1a7cd4e58b2116cdd0
psegenmark <psegenmark@gmail.com>
2017-12-09 16:48:41 -0800

    doneanddnoen

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
+mysql://b9e28e3df8d24d:f004ae51@us-cdbr-iron-east-05.cleardb.net/heroku_83af1219e198553?reconnect=true
 var connection = mysql.createConnection({
   host: "us-cdbr-iron-east-05.cleardb.net",
   user: "b9e28e3df8d24d",
   password: "f004ae51",
-  database: "heroku_83af1219e198553",
-  port: "3306"
+  database: "heroku_83af1219e198553"
 });
 
 connection.connect(function(err) {

703fc352fe5a77fc7770c8e2c19d849c013c2b61
psegenmark <psegenmark@gmail.com>
2017-12-09 21:11:30 -0800

    update

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
+var mysql2 = require("mysql2");
 
-var connection = mysql.createConnection("mysql://b6b066533a7665:0eaeab45@us-cdbr-iron-east-05.cleardb.net/heroku_3d889581badb7b0?reconnect=true");
+var connection = mysql2.createConnection("mysql2://b6b066533a7665:0eaeab45@us-cdbr-iron-east-05.cleardb.net/heroku_3d889581badb7b0?reconnect=true");
 
 connection.connect(function(err) {
   if (err) {
